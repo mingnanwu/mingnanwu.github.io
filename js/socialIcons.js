071096b730ca4a8cf6e0121e961af849265fffd8
mingnanwu <mingnanwu@gmail.com>
2015-05-12 21:28:39 -0700

    Added somebutton with font awesome!

---
+++ js/socialIcons.js
@@ -0,0 +1,26 @@
+/**
+ * Created by mingnanwu on 5/12/15.
+ */
+var ButtonToolbar = ReactBootstrap.ButtonToolbar,
+    ButtonGroup = ReactBootstrap.ButtonGroup,
+    Button = ReactBootstrap.Button,
+    Glyphicon = ReactBootstrap.Glyphicon;
+
+
+var icons = (
+        //
+        //<ButtonToolbar>
+        //<ButtonGroup>
+        //    <Button bsSize='large'><Glyphicon glyph='star' /> Star</Button>
+        //    <Button bsSize='xsmall'><Glyphicon glyph='star' /> Star</Button>
+        //</ButtonGroup>
+        //</ButtonToolbar>
+    <div className="btn-group">
+        <a className="btn btn-default" href="#"><i className="fa fa-align-left"></i></a>
+        <a className="btn btn-default" href="#"><i className="fa fa-align-center"></i></a>
+        <a className="btn btn-default" href="#"><i className="fa fa-refresh fa-spin fa-4x"></i></a>
+        <a className="btn btn-default" href="#"><i className="fa fa-align-justify"></i></a>
+    </div>
+);
+
+React.render(icons, document.getElementById('socialIcons'));
