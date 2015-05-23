f0be48b291bf73a22d7ced9b2ed3b499d7adbbda
mingnanwu <mingnanwu@gmail.com>
2015-05-23 13:42:25 -0700

    Added introbox, animation and lighter color

--- js/MainBox.js
+++ js/MainBox.js
@@ -1,3 +1,23 @@
 /**
  * Created by mingnanwu on 5/23/15.
  */
+/** @jsx React.DOM */
+
+
+// This is more complex example that uses two components -
+// a service chooser form, and the individual services inside it.
+
+
+var IntroBox = React.createClass({
+    render: function() {
+        return (
+            <div className="commentBox">
+                Hello, world! I am a IntroBox.
+            </div>
+        );
+    }
+});
+React.render(
+    <IntroBox />,
+    document.getElementById('mainBox')
+);
