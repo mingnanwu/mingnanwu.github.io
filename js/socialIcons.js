351a689760e03a4c00fed3789972a65a43d56fc3
v-kevwu <mingnanwu@gmail.com>
2015-05-26 12:36:30 -0700

    added tooltip on buttons

--- js/socialIcons.js
+++ js/socialIcons.js
@@ -4,25 +4,39 @@
 var ButtonToolbar = ReactBootstrap.ButtonToolbar,
     ButtonGroup = ReactBootstrap.ButtonGroup,
     Button = ReactBootstrap.Button,
-    Glyphicon = ReactBootstrap.Glyphicon;
-
+    Glyphicon = ReactBootstrap.Glyphicon,
+    Tooltip = ReactBootstrap.Tooltip,
+    //OverlayTrigger = ReactBootstrap.OverlayTrigger;
+    OverlayTrigger = ReactBootstrap.OverlayTrigger;
 
 var icons = (
-        //
-        //<ButtonToolbar>
-        //<ButtonGroup>
-        //    <Button bsSize='large'><Glyphicon glyph='star' /> Star</Button>
-        //    <Button bsSize='xsmall'><Glyphicon glyph='star' /> Star</Button>
-        //</ButtonGroup>
-        //</ButtonToolbar>
         <div className="btn-group">
-            <a className="btn btn-default" href="https://github.com/mingnanwu"><i className="fa fa-github fa-4x"></i></a>
-            <a className="btn btn-default" href="https://www.linkedin.com/in/mingnanwu"><i className="fa fa-linkedin-square fa-4x"></i></a>
-            <a className="btn btn-default" href="https://google.com/+wumingnan"><i className="fa fa-google-plus fa-4x"></i></a>
-            <a className="btn btn-default" href="mailto:mingnanwu@gmail.com"><i className="fa fa-envelope-o fa-4x"></i></a>
+            <OverlayTrigger placement='top' overlay={<Tooltip>My github page</Tooltip>}>
+                <a className="btn btn-default" href="https://github.com/mingnanwu"><i className="fa fa-github fa-4x"></i></a>
+            </OverlayTrigger>
+            <OverlayTrigger placement='top' overlay={<Tooltip>Connect me on LinedIn</Tooltip>}>
+                <a className="btn btn-default" href="https://www.linkedin.com/in/mingnanwu"><i className="fa fa-linkedin-square fa-4x"></i></a>
+            </OverlayTrigger>
+            <OverlayTrigger placement='top' overlay={<Tooltip>Google+</Tooltip>}>
+                <a className="btn btn-default" href="https://google.com/+wumingnan"><i className="fa fa-google-plus fa-4x"></i></a>
+            </OverlayTrigger>
+            <OverlayTrigger placement='top' overlay={<Tooltip><strong>Email me!</strong> Check this info.</Tooltip>}>
+                <a className="btn btn-default" href="mailto:mingnanwu@gmail.com"><i className="fa fa-envelope-o fa-4x"></i></a>
+            </OverlayTrigger>
         </div>
 
+    //<ButtonToolbar>
+    //
+    //    <Button>Submit</Button>
+    //    <Button>Cancel</Button>
+    //    <OverlayTrigger placement='left' overlay={<Tooltip><strong>Holy guacamole!</strong> Check this info.</Tooltip>}>
+    //        <Button bsStyle='default'>Holy guacamole!</Button>
+    //    </OverlayTrigger>
+    //</ButtonToolbar>
+
+
 );
+
 
 
 
