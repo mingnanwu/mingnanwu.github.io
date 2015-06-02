56fa016d9e883873d2335893fb2156f87c47290e
v-kevwu <mingnanwu@gmail.com>
2015-06-02 10:04:00 -0700

    responsive for phone

--- js/nav.js
+++ js/nav.js
@@ -9,16 +9,16 @@ var Navbar = ReactBootstrap.Navbar,
 
 var navbarInstance = (
     <div>
-    <Navbar brand='Under Constraction' inverse toggleNavKey={0}>
+    <Navbar brand='' inverse toggleNavKey={0}>
         <Nav right eventKey={0}> {/* This is the eventKey referenced */}
-            <NavItem eventKey={1} href='#'>Link</NavItem>
-            <NavItem eventKey={2} href='#'>Link</NavItem>
-            <DropdownButton eventKey={3} title='Dropdown'>
-                <MenuItem eventKey='1'>Action</MenuItem>
-                <MenuItem eventKey='2'>Another action</MenuItem>
-                <MenuItem eventKey='3'>Something else here</MenuItem>
+            <NavItem eventKey={1} href='#'></NavItem>
+            <NavItem eventKey={2} href='#'></NavItem>
+            <DropdownButton eventKey={3} title=''>
+                <MenuItem eventKey='1'></MenuItem>
+                <MenuItem eventKey='2'></MenuItem>
+                <MenuItem eventKey='3'></MenuItem>
                 <MenuItem divider />
-                <MenuItem eventKey='4'>Separated link</MenuItem>
+                <MenuItem eventKey='4'></MenuItem>
             </DropdownButton>
         </Nav>
     </Navbar>
