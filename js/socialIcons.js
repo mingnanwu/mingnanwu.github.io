4f7604fa5b5d124c799b538613e79ffb228b95c7
mingnan wu <mingnanwu@gmail.com>
2015-06-14 16:34:07 -0700

    social icon fix, before color change

--- js/socialIcons.js
+++ js/socialIcons.js
@@ -11,19 +11,19 @@ var ButtonToolbar = ReactBootstrap.ButtonToolbar,
 
 var icons = (
         <div className="btn-group">
-            <OverlayTrigger placement='top' overlay={<Tooltip>My github page</Tooltip>}>
+            <OverlayTrigger placement='top' overlay={<Tooltip><strong>My github page</strong></Tooltip>}>
                 <a className="btn btn-default" href="https://github.com/mingnanwu"><i className="fa fa-github fa-4x"></i></a>
             </OverlayTrigger>
-            <OverlayTrigger placement='top' overlay={<Tooltip>Connect me on LinedIn</Tooltip>}>
+            <OverlayTrigger placement='top' overlay={<Tooltip><strong>Connect me on LinedIn</strong></Tooltip>}>
                 <a className="btn btn-default" href="https://www.linkedin.com/in/mingnanwu"><i className="fa fa-linkedin-square fa-4x"></i></a>
             </OverlayTrigger>
-            <OverlayTrigger placement='top' overlay={<Tooltip>Google+</Tooltip>}>
+            <OverlayTrigger placement='top' overlay={<Tooltip><strong>Google+</strong></Tooltip>}>
                 <a className="btn btn-default" href="https://google.com/+wumingnan"><i className="fa fa-google-plus fa-4x"></i></a>
             </OverlayTrigger>
             <OverlayTrigger placement='top' overlay={<Tooltip><strong>Email me!</strong></Tooltip>}>
                 <a className="btn btn-default" href="mailto:mingnanwu@gmail.com"><i className="fa fa-envelope-o fa-4x"></i></a>
             </OverlayTrigger>
-            <OverlayTrigger placement='top' overlay={<Tooltip><strong>Email me!</strong> Check this info.</Tooltip>}>
+            <OverlayTrigger placement='top' overlay={<Tooltip><strong>My Resume</strong></Tooltip>}>
                 <a className="btn btn-default" href="https://www.dropbox.com/s/rjwdmqtsz0okyjw/resume%202015.pdf?dl=0"><i className="fa fa-file fa-4x"></i></a>
             </OverlayTrigger>
         </div>
